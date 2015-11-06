d734c26272fb0a4de8b7bd2d75e2ef7bb63e857f
OBellon <OscarBellon96@hotmail.com>
2015-11-06 12:42:03 +0100

    Almost

---
+++ src/containers/App.jsx
@@ -0,0 +1,27 @@
+import Article from './Article';
+import Footer from './Footer';
+import Header from './Header';
+import Nav from './Nav';
+
+
+import React, { Component } from 'react';
+
+export default class App extends Component {
+
+	constructor(props){
+		super(props);
+	}
+
+  render() {
+    return (
+      <div>
+  	    /*<Header/>
+  	    <Nav/>
+  	    <Article/>
+  	    <Footer/>*/
+  	    Hola
+      </div>
+    );
+  }
+  
+}
--- src/containers/Article.jsx
+++ src/containers/Article.jsx
@@ -5,10 +5,10 @@ export class Article extends React.Component {
 	render() {
 		return (
 			<article className='article row'>
-    			/*<div className="col-xs-12">
+    			<div className="col-xs-12">
       				<List name='Lista 1' undoTasks={15}/>
         			<List name='Lista 2' undoTasks={3}/>
-      			</div>*/
+      			</div>
       		</article>
 		);
 	}
